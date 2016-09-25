632da1ac90fbe82942276855ee789f7eb0c7daf5
Zhihua Yang <yzh813@gmail.com>
2016-09-24 17:00:58 -0700

    fix/turn off worker

--- server/workers.js
+++ server/workers.js
@@ -8,12 +8,13 @@ module.exports = {
     var currentDate = new Date().toJSON().slice(0,10);
     //var url = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=EHAU7JI8sxVlrnoDKQD0Ylr01o9cdudk&startDateTime=${currentDate}T00:00:00Z&size=50&countryCode=US`;
     var url = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=EHAU7JI8sxVlrnoDKQD0Ylr01o9cdudk&size=50&countryCode=US`;
-    Request.get(url).then(function(response) {
-      response.body._embedded.events.forEach(function(event) {
-        console.log('response',event.description);
-        console.log('================')
-      })
-    });
+    // Request.get(url).then(function(response) {
+    //   response.body._embedded.events.forEach(function(event) {
+    //     console.log('response',event.description);
+    //     console.log('================')
+    //   })
+    // });
+    console.log('worker is working')
   }
 
 }
