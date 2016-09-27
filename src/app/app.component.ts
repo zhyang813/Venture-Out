3fa2e9cea09035e322de59f8d8d2b6fe43528a83
hoonbae <hoonbae244@gmail.com>
2016-09-28 03:04:45 +0900

    applied style changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 
-// import { HeaderComponent } from './header.component';
-// import { SearchboxComponent } from './searchbox';
-// import { SearchresultsComponent } from './searchresults';
+
+import { SearchboxComponent } from './searchbox';
+import { SearchresultsComponent } from './searchresults'
+
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,29 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AUTH_PROVIDERS } from 'angular2-jwt';
 
+import { routing } from "./app.routing";
+
+// app components
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header.component';
 import { SearchboxComponent } from './searchbox/searchbox.component';
 import { routing } from './app.routing';
 import { SearchresultsComponent } from './searchresults/searchresults.component';
 import { AuthService } from './auth/auth.service';
 
+import { SearchresultsComponent } from './searchresults/searchresults.component';
+
+// ng2 material design modules
+import { MdToolbarModule } from '@angular2-material/toolbar';
+import { MdInputModule } from '@angular2-material/input';
+import { MdButtonModule } from '@angular2-material/button';
+import { MdCardModule } from '@angular2-material/card';
+import { MdMenuModule } from '@angular2-material/menu';
+import { MdSidenavModule } from '@angular2-material/sidenav';
+import { MdIconModule } from '@angular2-material/icon';
+
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     SearchboxComponent,
     SearchresultsComponent
   ],
@@ -22,7 +34,14 @@ import { AuthService } from './auth/auth.service';
     BrowserModule,
     FormsModule,
     HttpModule,
-    routing
+    routing,
+    MdToolbarModule.forRoot(),
+    MdInputModule.forRoot(),
+    MdCardModule,
+    MdButtonModule,
+    MdMenuModule,
+    MdSidenavModule,
+    MdIconModule
   ],
   providers: [AUTH_PROVIDERS, AuthService],
   bootstrap: [AppComponent]
