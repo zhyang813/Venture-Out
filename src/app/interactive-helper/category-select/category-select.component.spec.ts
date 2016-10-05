265fb35db39ace80baac6e050e3ea0b6770699f3
ItsDavidThai <itsdavidthai@gmail.com>
2016-10-05 14:37:16 -0700

    add basic test config to category select component

--- src/app/interactive-helper/category-select/category-select.component.spec.ts
+++ src/app/interactive-helper/category-select/category-select.component.spec.ts
@@ -2,10 +2,34 @@
 
 import { TestBed, async } from '@angular/core/testing';
 import { CategorySelectComponent } from './category-select.component';
+// import { routing } from '../../app.routing';
+import { Router } from '@angular/router';
+
 
 describe('Component: CategorySelect', () => {
-  it('should create an instance', () => {
-    let component = new CategorySelectComponent();
-    expect(component).toBeTruthy();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        CategorySelectComponent
+      ],
+      imports: [
+        Router
+      ]
+    });
   });
+
+  describe('onLike Method', () => {
+    it('should add liked categories to liked array', () => {
+      let fixture = TestBed.createComponent(CategorySelectComponent);
+      let comp = fixture.componentInstance;
+      expect(comp.liked.length).toBe(0)
+      // component.onLike('food')
+      // component.onLike('music')
+      // component.onLike('dance')
+      // expect(component.liked[0]).toBe('food')
+      // expect(component.liked[1]).toBe('music')
+      // expect(component.liked[2]).toBe('dance')
+
+    })
+  })
 });
