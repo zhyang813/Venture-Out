f9c8eca4b01553cf8cd94f49114adf7c85ab06fa
Zhihua Yang <yzh813@gmail.com>
2016-10-05 14:51:44 -0700

    Server api and db tests added

--- server/eventHandler.js
+++ server/eventHandler.js
@@ -12,6 +12,7 @@ module.exports = {
         console.log("New event created error", err);
       } else {
         console.log('Successful created an event!')
+        return event;
       }
     });
   },
@@ -26,7 +27,7 @@ module.exports = {
       if (err) {
         console.log("mongo findOne event err: ", err);
       } else if (!event) {
-          callback();
+        callback();
       }
     });
   },
@@ -52,14 +53,14 @@ module.exports = {
       return (
         services.geocoder.geocode(zipCode)
         .then(function(result){
-            var country;
-            var city = result[0].city.split(' ').join('_');
-            if(result[0].country === 'United States'){
-              country = 'America'
-            }
-           return `${country}/${city}`
+          var country;
+          var city = result[0].city.split(' ').join('_');
+          if(result[0].country === 'United States'){
+            country = 'America'
+          }
+          return `${country}/${city}`
         })
-      )
+        )
     },
     getEvents = function(zip){
       return Event.where('genre')
@@ -78,26 +79,6 @@ module.exports = {
     getLocation(zipCode)
     .then(getEvents)
 
-
   }
-
-  // frontPageRecommendation: function(req, res) {
-
-  //   var currentDate = new Date(new Date().getTime() + 2 * 24 * 60 * 60 * 1000).toJSON().slice(0,10);
-  //   var currentTime = currentDate + 'T00:00:00Z'
-
-  //   console.log('server currentTime', currentTime);
-
-  //   Event.find({eventStartTime: currentTime}, function(err, events) {
-  //     if (err) {
-  //       console.log("New event created error", err);
-  //       res.status(500).send({error: err});
-  //     } else {
-  //       console.log('server', events)
-  //       //res.json(events);
-  //     }
-  //   });
-
-  // }
 
 }
