ef418bf58ca088b6c5a9e7ae833620a317b7d9c7
Zhihua Yang <yzh813@gmail.com>
2016-10-11 09:18:56 -0700

    Restyling on signup exp

--- src/app/user-page/user-page.component.ts
+++ src/app/user-page/user-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 import { UserPageService } from './user-page.service';
-import { Observable, Subscriber, Subscription } from 'rxjs/Rx';
+// import { Observable, Subscriber, Subscription } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-user-page',
@@ -14,26 +14,25 @@ export class UserPageComponent implements OnInit {
   zipCode: any;
   interests: any;
 
-  constructor(private auth: AuthService,
+  constructor (private auth: AuthService,
               private userService: UserPageService) {
-    this.userService.getFavorites().subscribe(
+    this.userService.getFavorites().subscribe (
       data => this.favorites = data
     );
     // this.userService.getZipCode().subscribe(data => this.zipCode = data)
     // this.userService.getInterests().subscribe(data => this.interests = data)
     this.userService.getZipAndInterests().subscribe(
       data => {
-        this.zipCode = data[0]
-        this.interests = data[1]
+        this.zipCode = data[0];
+        this.interests = data[1];
       }, null,
       () => {
-        console.log(this.zipCode, this.interests)
+        console.log(this.zipCode, this.interests);
         this.userService.getRecommendations(this.zipCode, this.interests).subscribe(
           (data) => this.recommendations = data
-        )
+        );
       }
-
-    )
+    );
 
   }
 
