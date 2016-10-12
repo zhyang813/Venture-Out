2e7be4f2820141a907ab14d40a49d771d326814b
ItsDavidThai <itsdavidthai@gmail.com>
2016-10-12 16:23:01 -0700

    add documentation to event handler

--- server/eventHandler.js
+++ server/eventHandler.js
@@ -6,7 +6,7 @@ var async = require('async');
 module.exports = {
 
   // Add new Event
-  addEvent: function(event){
+  addEvent: function(event) {
     Event.create(event, function(err, event) {
       if (err) {
         console.log("New event created error", err);
@@ -14,13 +14,10 @@ module.exports = {
         console.log('Successful created an event!')
       }
     });
-
   },
 
   // Find a specific event using eventId
   findEventById: function(eventId, callback) {
-
-    console.log('findEvent', eventId);
 
     Event.findOne({'eventId': eventId}, function(err, event){
       // console.log('event found?', event);
@@ -30,7 +27,6 @@ module.exports = {
         callback();
       }
     });
-
   },
 
   // Get all the events
@@ -43,12 +39,10 @@ module.exports = {
         res.json(events);
       }
     });
-
   },
 
   // returns promise for chaining, does not take requests or send responses
   getLocationFromZipPromise: function(zipCode) {
-
     return (
       services.geocoder.geocode(zipCode)
       .then(function(result){
@@ -62,24 +56,23 @@ module.exports = {
           city: city
         }
       })
-
     )
-
   },
 
   // returns promise for chaining, does not take requests or send responses
   // locationData is the result from the geocoder api
-  // if genre is included search but category, if not get all events by city
+  // if genre is included search by category, if not get all events by city
   getEventsPromise: function(locationData, genre, limit) {
-
     var city = locationData.city.split('_').join(' ');
 
-    if(!genre){
+    if(!genre) {
+
       return Event.where('address.city').eq(city).sort('createdAt').limit(20)
+
     } else {
       // using a regex to find categories in searches that have multiple words
       var regex = `\\bgenre\\b`.replace('genre', genre).split('"').join('');
-      console.log(regex, 'this is in get events')
+
       return Event.find({
         $and: [
           {'address.city': city},
@@ -91,70 +84,69 @@ module.exports = {
 
   // Use this method to get events by zip requires the zipcode in req params
   getEventsByZip: function(req, res) {
-
-    var numberOfEvents = Number.parseInt.call(this, req.params.amount)
+    // ignore for now may be used later
+    // var numberOfEvents = Number.parseInt.call(this, req.params.amount)
     var zipCode = req.params.zip;
 
+    // use promises in a chain
     this.getLocationFromZipPromise(zipCode)
     .then(this.getEventsPromise)
-    .then(function(result){
+    .then(function(result) {
       res.json(result)
     })
-    .catch(function(err){
+    .catch(function(err) {
       res.json(err)
     })
   },
 
   // grab events by categories and zip
   getEventsByCategoriesAndZip: function(req, res) {
-
-    var numberOfEvents = Number.parseInt.call(this, req.params.amount)
     var zipCode = req.params.zip;
-    console.log(req.params)
     var that = this;
 
     getMultipleEvents = function(locationData) {
 
-     var events = [];
+      var events = [];
 
       // JSON is stringified in the request. slice off start/end quotations
       var interests = req.params.name.slice(1, req.params.name.length - 1).split(',')
-      var limit = Math.floor(13/interests.length);
+
       // this runs async functions in order
       async.waterfall([
-        function(Outercallback){
+        // waterfall takes in an array of async operations and runs them in async order the second arguement is a function that runs when every async function is ran
+        // outerCallback is for async.waterfall to know when a function has finished running
+        function(outerCallback) {
+        // async map will getEvents based on every interest in the interests array
           async.map(interests, function(interest, callback) {
-            console.log(interest, locationData, '!------')
             that.getEventsPromise(locationData, interest, 4)
-            .then(function(result){
-              console.log(result, 'grabbing each event')
+            .then(function(result) {
               callback(null, result)
             })
-            .catch(function(err){
-              console.log(err)
+            .catch(function(err) {
               res.json(err)
             });
-          }, function(err, result){
-            // console.log('query Successful')
-            Outercallback(err, result)
+          }, function(err, result) {
+            // signals the current async function is done running
+            outerCallback(err, result)
           })
         }
-        ],
-        function(err, result){
+      ],
+        // run this function after all async functions have ran
+        function(err, result) {
           console.log('prmoise chain successful')
-        // flatten arrays
+        // flatten arrays into one array
         result = result.reduce(function(store, next) {
           next.forEach(function(element) {
             store.push(element)
           })
-          return store
-        }, [])
-        res.json(result)
-      }
-      )
+          return store }, [])
 
+          res.json(result)
+        }
+      )
     }
 
+    // use promises in a chain
     this.getLocationFromZipPromise(zipCode)
     .then(getMultipleEvents)
 
